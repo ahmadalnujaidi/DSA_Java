217c51ffdb60adf485a37d146aa271604c40e5bc
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2024-09-30 09:08:54 +0300

    time complexity

---
+++ Algorithm Testing/TimeComplexityDemo.java
@@ -0,0 +1,21 @@
+public class TimeComplexityDemo{
+    public static void main(String[] args) {
+        double now = System.currentTimeMillis();
+
+        TimeComplexityDemo test = new TimeComplexityDemo();
+        System.out.println(test.findSum(99999));
+
+        System.out.println("time taken: " + (System.currentTimeMillis() - now) + "millisecs.");
+    }
+    public int findSum(int n){
+        return n * (n + 1) / 2;
+    };
+
+    // public int findSum(int n){
+    //     int sum = 0;
+    //     for (int i = 1; i <= n; i++){
+    //         sum = sum + i;
+    //     }
+    // return sum;
+    // }
+}
